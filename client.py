5afb6ed817009ccbbb0e96d7a619415b760032a5
Lucas Jandrew <fapiko@gmail.com>
2012-04-24 23:32:40 -0700

    Initial commit

---
+++ client.py
@@ -0,0 +1,14 @@
+import zmq
+
+context = zmq.Context()
+
+sock = context.socket(zmq.SUB)
+sock.connect('tcp://127.0.0.1:2000')
+
+# This tells the consumer to drop all messages which
+# don't begin with the text ARNOLD
+sock.setsockopt(zmq.SUBSCRIBE, 'ARNOLD')
+
+while True:
+	input = sock.recv()
+	print input
